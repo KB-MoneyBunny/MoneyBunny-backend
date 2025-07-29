85062ba499f18c601c0665b4963d7ac219e135ee
Jeseung Yoon <109639745+yjs1228@users.noreply.github.com>
2025-07-29 16:31:24 +0900

    Feat: 사용자 정책 조건 수정 및 수정한 조건에 알맞는 정책 목록 저장 기능 구현

--- src/main/java/org/scoula/userPolicy/service/UserPolicyService.java
+++ src/main/java/org/scoula/userPolicy/service/UserPolicyService.java
@@ -1,9 +1,12 @@
 package org.scoula.userPolicy.service;
 
+import org.scoula.userPolicy.domain.UserPolicyConditionVO;
 import org.scoula.userPolicy.dto.UserPolicyDTO;
 
 import java.util.List;
 
 public interface UserPolicyService {
     UserPolicyDTO saveUserPolicyCondition(String username, UserPolicyDTO userPolicyDTO);
+
+    UserPolicyDTO updateUserPolicyCondition(String username, UserPolicyDTO userPolicyDTO);
 }
