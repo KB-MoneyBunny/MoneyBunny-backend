e651fc7b7f5476e66f2c6ab48d35d8cf71701d30
inredzero <caesete0524@naver.com>
2025-07-30 11:31:46 +0900

    ♻️refactor: insertAccountTransactions 일괄 처리 추가 #27

--- src/main/java/org/scoula/codef/mapper/AccountTransactionMapper.java
+++ src/main/java/org/scoula/codef/mapper/AccountTransactionMapper.java
@@ -1,7 +1,17 @@
 package org.scoula.codef.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.scoula.codef.domain.AccountTransactionVO;
+
+import java.util.List;
+import java.util.Set;
 
 public interface AccountTransactionMapper {
     void insertAccountTransaction(AccountTransactionVO vo);
+
+    String findLastTransactionDateByAccountId(Long accountId);
+
+    Set<String> findAllTxKeyByAccountIdFromDate(@Param("accountId") Long accountId, @Param("startDate") String startDate);
+
+    void insertAccountTransactions(List<AccountTransactionVO> batch);
 }
