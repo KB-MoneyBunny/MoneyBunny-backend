9a6b6aeb0eed5855c57558b1954349d134b459be
inredzero <caesete0524@naver.com>
2025-07-31 13:20:52 +0900

    ⚡️feat: asset 도메인/서비스 신규 추가 #43

---
+++ src/main/java/org/scoula/asset/controller/AssetController.java
@@ -0,0 +1,79 @@
+package org.scoula.asset.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.scoula.asset.domain.AccountSummaryVO;
+import org.scoula.asset.domain.AccountTransactionVO;
+import org.scoula.asset.domain.CardSummaryVO;
+import org.scoula.asset.domain.CardTransactionVO;
+import org.scoula.asset.dto.AssetSummaryResponse;
+import org.scoula.asset.service.AssetService;
+import org.scoula.common.dto.PageResponse;
+import org.scoula.push.dto.response.NotificationResponse;
+import org.scoula.security.account.domain.CustomUser;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/asset")
+@RequiredArgsConstructor
+@Api(tags = "자산 관련 API", description = "계좌, 카드, 자산 요약 등 자산 관련 API 제공")
+public class AssetController {
+
+    private final AssetService assetService;
+
+    @ApiOperation(value = "자산 요약 조회", notes = "내 자산(계좌 합), 이번달 카드 사용금액, 계좌/카드 요약을 조회합니다.")
+    @GetMapping("/summary")
+    public ResponseEntity<AssetSummaryResponse> getSummary() {
+        String loginId = "hong1";
+        AssetSummaryResponse summary = assetService.getSummary(loginId);
+        return ResponseEntity.ok(summary);
+    }
+
+    // 계좌 전체 목록
+    @ApiOperation(value = "전체 계좌 목록", notes = "사용자의 전체 계좌 리스트를 반환합니다.")
+    @GetMapping("/accounts")
+    public ResponseEntity<List<AccountSummaryVO>> getAccounts() {
+        String loginId = "hong1";
+        return ResponseEntity.ok(assetService.getAccounts(loginId));
+    }
+
+    // 카드 전체 목록
+    @ApiOperation(value = "전체 카드 목록", notes = "사용자의 전체 카드 리스트를 반환합니다.")
+    @GetMapping("/cards")
+    public ResponseEntity<List<CardSummaryVO>> getCards() {
+        String loginId = "hong1";
+        List<CardSummaryVO> cards = assetService.getCards(loginId);
+        return ResponseEntity.ok(cards);
+    }
+
+    // 계좌 거래내역 조회(페이징 20개씩)
+    @ApiOperation(value = "계좌 거래내역 페이징 조회(최근순)", notes = "특정 계좌의 거래내역을 페이지별로 조회합니다. page(0부터), size 지정 가능(default:20)")
+    @GetMapping("/accounts/{accountId}/transactions")
+    public ResponseEntity<PageResponse<AccountTransactionVO>> getAccountTransactions(
+            @PathVariable Long accountId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size
+            ) {
+        PageResponse<AccountTransactionVO> resp = assetService.getAccountTransactions(accountId, page, size);
+        return ResponseEntity.ok(resp);
+    }
+
+    // 카드 거래내역 조회(페이징 20개씩)
+    @ApiOperation(value = "카드 거래내역 페이징 조회(최근순)", notes = "특정 카드의 거래내역을 페이지별로 조회합니다. page(0부터), size 지정 가능(default:20)")
+    @GetMapping("/cards/{cardId}/transactions")
+    public ResponseEntity<PageResponse<CardTransactionVO>> getCardTransactions(
+            @PathVariable Long cardId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size
+            ) {
+        PageResponse<CardTransactionVO> resp = assetService.getCardTransactions(cardId, page, size);
+        return ResponseEntity.ok(resp);
+    }
+
+}
