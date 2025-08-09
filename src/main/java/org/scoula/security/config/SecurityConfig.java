b12b6c0b94bc4894ff331f64ba00e231746a025c
Sangil Park <65345814+sangil6372@users.noreply.github.com>
2025-08-09 10:00:24 +0900

    Update SecurityConfig.java

--- src/main/java/org/scoula/security/config/SecurityConfig.java
+++ src/main/java/org/scoula/security/config/SecurityConfig.java
@@ -108,7 +108,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/member/**").permitAll()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/asset/**").permitAll()
-                .antMatchers("/api/application/incomplete").permitAll()
+                .antMatchers("/api/policy-interaction/application/incomplete").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/policy/*").permitAll() // 공유 URL 로그인 X
                 .antMatchers(HttpMethod.GET, "/api/policy/detail/**").permitAll()
                 .antMatchers("/api/policy/**").authenticated() // 정책 API 임시 허용
