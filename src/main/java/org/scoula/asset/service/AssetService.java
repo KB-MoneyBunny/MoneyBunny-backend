6cd0999c146fadee759cb73e7ef1ecf9ab6726ea
Jeseung Yoon <109639745+yjs1228@users.noreply.github.com>
2025-08-12 16:47:23 +0900

    Feat: 한국산업인력공단 카드 결제내역 존재 여부 확인 API 구현 #41

--- src/main/java/org/scoula/asset/service/AssetService.java
+++ src/main/java/org/scoula/asset/service/AssetService.java
@@ -231,4 +231,8 @@ public class AssetService {
         // userId로 바로 월세 거래내역 존재 여부 확인
         return assetAccountTransactionMapper.existsRentTransactionByUserId(userId);
     }
+
+    public boolean existsHrdKoreaCardTransaction(Long userId) {
+        return assetCardTransactionMapper.existsHrdKoreaCardTransactionByUserId(userId);
+    }
 }
